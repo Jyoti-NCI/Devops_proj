78b02c3505b82b4e12363849faa1e264e1a614ac
EC2 Default User <ec2-user@ip-172-31-10-123.eu-west-1.compute.internal>
2025-03-27 23:46:53 +0000

    added allowed host in settings.py

--- Devops_proj/settings.py
+++ Devops_proj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'm=n85xtlz$c7b-jt@og-s!s+!v&s)s(lv@1!zpdepoqq7ty+#2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['x24104558expensetracker1-env.eba-kvnjcenq.eu-west-1.elasticbeanstalk.com']
 
 
 # Application definition
