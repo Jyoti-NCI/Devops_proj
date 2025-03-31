2e3040939a6fd0fd8ac5561d3515ff58c4f9b43d
EC2 Default User <ec2-user@ip-172-31-10-123.eu-west-1.compute.internal>
2025-03-31 16:46:31 +0000

    dev

--- Devops_proj/settings.py
+++ Devops_proj/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
-
+#test
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'm=n85xtlz$c7b-jt@og-s!s+!v&s)s(lv@1!zpdepoqq7ty+#2'
 
