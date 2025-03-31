3c53aa78e776e1260d5232128d58f8a23127e92c
EC2 Default User <ec2-user@ip-172-31-10-123.eu-west-1.compute.internal>
2025-03-31 17:24:30 +0000

    de1

--- Devops_proj/settings.py
+++ Devops_proj/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'm=n85xtlz$c7b-jt@og-s!s+!v&s)s(lv@1!zpdepoqq7ty+#2'
 DEBUG = True
 
 ALLOWED_HOSTS = ['x24104558expensetracker1-env.eba-kvnjcenq.eu-west-1.elasticbeanstalk.com',
-'172.31.2.101', '108.129.52.16',]
+'172.31.2.101', '108.129.52.16', '172.31.44.153',]
 
 # adding comments to rtest sona
 
